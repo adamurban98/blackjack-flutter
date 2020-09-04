e8ac12cb80dc74a91f561d50fa31d32d692ccab5
Adam Urban <adamurban98@gmail.com>
2020-09-04 12:26:30 +0200

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:math';
 
 import 'package:blackjack/hand.dart';
@@ -27,7 +26,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
